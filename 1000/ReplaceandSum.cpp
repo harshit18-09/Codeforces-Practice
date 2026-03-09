8d687fd5e18143a9dcd2d9e703e00e920cb7ecb1
HC18 <harshit.chugh18@gmail.com>
2026-03-10 05:18:56 +0530

    10-03-2026

--- 1000/ReplaceandSum.cpp
+++ 1000/ReplaceandSum.cpp
@@ -1,43 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<long long> a(n + 1), b(n + 1), suffix_max(n + 2, 0);
+
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) cin >> b[i];
+
+    for (int i = n; i >= 1; i--) {
+        suffix_max[i] = max({a[i], b[i], suffix_max[i + 1]});
+    }
+
+    vector<long long> pref(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        pref[i] = pref[i - 1] + suffix_max[i];
+    }
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << pref[r] - pref[l - 1] << (q == 0 ? "" : " ");
+    }
+    cout << "\n";
+}
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
-    while(t--){
-        int n, q;
-        cin>>n>>q;
-        vector<int> a(n);
-        vector<int> b(n);
-        for(int i=0; i<n; i++){
-            cin>>a[i];
-        }
-        for(int i=0; i<n; i++){
-            cin>>b[i];
-        }
-        vector<pair<int,int>> query(q);
-        for(int i=0; i<q; i++){
-            cin>>query[i].first>>query[i].second;
-        }
-        vector<int> final(n);
-        final[n-1] = max(a[n-1], b[n-1]);
-        for(int i=n-2; i>=0; i--){
-            final[i] = max(final[i+1], b[i]);
-        }
-        vector<int> ans(q);
-        for(int i=0; i<q; i++){
-            int l = query[i].first-1;
-            int r = query[i].second-1;
-            for(int j=l; j<=r; j++){
-                ans[i] += final[j];
-            }
-        }
-        for(int i=0; i<q; i++){
-            cout<<ans[i]<<" ";
-        }
-        cout<<endl;
+    cin >> t;
+    while (t--) {
+        solve();
     }
     return 0;
 }
